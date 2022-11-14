2d7342f8d96d506bcf94d0408fb2df4a100dd61e
Richard <54506990+RichLarIva@users.noreply.github.com>
2022-11-14 08:53:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -100,18 +100,7 @@
 
         </script>
 
-        <script>
-            function onKC(cb) {
-                var input = '';
-                var key = '38384040373937396665';
-                document.addEventListener('keydown', function (e) {
-                    input += ("" + e.keyCode);
-                    if (input === key) { return cb();}
-                    if (!key.indexOf(input)) return; input = ("" + e.keyCode);
-                });
-            }
-            onKC(function () { alert('Hello'); })
-        </script>
+
 
     </div>
 
